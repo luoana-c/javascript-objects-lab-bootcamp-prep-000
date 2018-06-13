3dc6ab83c26a02d0676a815159165150df6c1ab1
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-13 10:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var recipes = {eggs: '3'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  return Object.assign({}, object, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
