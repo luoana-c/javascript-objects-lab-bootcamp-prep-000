76a643ece3331b383da588bc8e28bc65c3f10c9d
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-13 10:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {eggs: '3'};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value;
   return object;
 }
